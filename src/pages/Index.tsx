356e9dd78c620040f8c5e1f7de243a7d66717162
enter-pro <github@enter.pro>
2026-03-02 21:00:23 +0000

    User edit code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -354,6 +354,23 @@ const Index = () => {
                   <div className="w-3 h-3 rounded-full bg-blue-100"></div>
                 </div>
                 <span>Missouri Based</span>
+                 <div className="flex justify-center">
+              <Dialog>
+                <DialogTrigger asChild>
+                  <button className="bg-blue-600 hover:bg-red-500 text-white font-bold text-sm px-6 py-3 rounded-lg shadow-lg transition transform hover:scale-105">
+                    RICP® | CLTC | MMC
+                  </button>
+                </DialogTrigger>
+                <DialogContent className="max-w-3xl">
+                  <img 
+                    src="https://grazia-prod.oss-ap-southeast-1.aliyuncs.com/resources/uid_100009141/aa41.webp" 
+                    alt="Insurance License Certificate" 
+                    className="w-full h-auto rounded-lg"
+                    crossOrigin="anonymous"
+                  />
+                </DialogContent>
+              </Dialog>
+            </div>
               </div>
             </div>
           </div>
