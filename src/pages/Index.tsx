90af891f8b4f2281569fe7dccaae66ee26adb5cf
enter-pro <github@enter.pro>
2026-03-02 21:08:42 +0000

    User edit code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -354,7 +354,7 @@ const Index = () => {
                   <div className="w-3 h-3 rounded-full bg-blue-100"></div>
                 </div>
                 <span>Mission Driven</span>
-              </div> </n>
+              </div> </n> <br />
               <div className="flex justify-center">
               <Dialog>
                 <DialogTrigger asChild>
