85c95c3e8c901fe481069fee1989b4b7eb9a2ce8
enter-pro <github@enter.pro>
2026-03-02 21:46:12 +0000

    User edit code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -418,7 +418,7 @@ const Index = () => {
               <h3 className="text-xl font-bold mb-4">CONTACT</h3>
               <p className="mb-2">Michael Thomas</p>
               <p className="mb-2">RICP® | CLTC | MMC</p>
-              <p><a href="tel:816-752-4944" className="text-2xl font-bold text-white hover:text-red-500 transition">
+              <p><a href="tel:816-752-4944" onClick={(e)=>{e.preventDefault(); window.location.href = "mailto:mthomas@krs.insure";}} className="text-2xl font-bold text-white hover:text-red-500 transition">
                 816-752-4944
               </a></p>
                <p><a href="mailto:mthomas@krs.insure" className="text-2xl font-bold text-white hover:text-red-500 transition">
