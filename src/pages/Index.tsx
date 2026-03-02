a4a0233e725736a921a8bf9dfae76259af5e81ae
enter-pro <github@enter.pro>
2026-03-02 21:40:06 +0000

    User edit code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -421,7 +421,7 @@ const Index = () => {
               <p><a href="tel:816-752-4944" className="text-2xl font-bold text-white hover:text-red-500 transition">
                 816-752-4944
               </a></p>
-              <p><a href="mailto:mthomas@krs.insure" className="text-2xl font-bold text-white hover:text-red-500 transition">
+              <p><a href="mailto:michael@example.com" className="text-2xl font-bold text-white hover:text-red-500 transition">
                 mthomas@krs.insure
               </a></p>
             </div>
