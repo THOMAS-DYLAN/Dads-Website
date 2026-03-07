24400bd45b73ab57fd018aeaa833ff8687c3f2b7
enter-pro <github@enter.pro>
2026-03-07 01:04:22 +0000

    User edit code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -220,7 +220,7 @@ const Index = () => {
               className="hidden lg:flex items-center gap-2 rounded-lg bg-red-500 px-6 py-3 font-bold text-white transition hover:bg-red-600 shadow-2g"
             >
               <button>
-                <Phone className="h-4 w-4" />
+                <Phone className="h-3 w-3" />
                 816-752-4944
               </button>
             </a>
