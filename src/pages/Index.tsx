5b9c6a84b46bf410e53b88c2c2794c9e1c5fd191
enter-pro <github@enter.pro>
2026-03-07 00:57:23 +0000

    User edit code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -67,7 +67,7 @@ const Index = () => {
             </div>
 
             {/* Desktop Navigation */}
-            <div className="items-center space-x-1">
+            <div className="space-x-1">
               {/* About */}
               <div
                 className="relative"
