c3ec83bdf6518ee0f66046b163158a591074c935
enter-pro <github@enter.pro>
2026-03-14 22:46:35 +0000

    User edit code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -75,7 +75,7 @@ const Index = () => {
         <div className="mx-auto max-w-7xl px-4">
           <div className="flex items-center justify-between h-20">
             <a href="/" className="text-2xl font-bold text-white ">
-              <House className="text-pink-100 object-fit: container hover:text-blue-500"/>
+              <House className="text-pink-500 object-fit: container hover:text-blue-500"/>
             </a>
             {/* Desktop Navigation */}
             <div className="hidden lg:flex items-center space-x-1">
