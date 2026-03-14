c029869059e76f26873bd90ce99781563d18fd18
enter-pro <github@enter.pro>
2026-03-14 22:42:35 +0000

    User edit code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ import {
   Phone,
   Menu,
   X,
-  Home
+  House,
   ChevronDown,
 } from "lucide-react";
 import { useState } from "react";
@@ -75,7 +75,7 @@ const Index = () => {
         <div className="mx-auto max-w-7xl px-4">
           <div className="flex items-center justify-between h-20">
             <a href="/" className="text-2xl font-bold text-white ">
-              <Home/>
+              <House/>
             </a>
             {/* Desktop Navigation */}
             <div className="hidden lg:flex items-center space-x-1">
