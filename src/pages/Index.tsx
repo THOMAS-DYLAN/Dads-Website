33a1037768afdb0a6821a0530202a714aa8a8ad2
enter-pro <github@enter.pro>
2026-03-19 02:11:50 +0000

    User edit code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -80,7 +80,7 @@ const Index = () => {
                 <img
                   src="https://grazia-prod.oss-ap-southeast-1.aliyuncs.com/resources/uid_100009141/b4ac.png"
                   alt="Service To Security"
-                  className="h-30 w-auto"
+                  className="h-22 w-auto"
                   crossOrigin="anonymous"
                 />
               </a>
