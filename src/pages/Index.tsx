1473a80e1b0a6c577848bfb7550d1df8c0c10b4d
enter-pro <github@enter.pro>
2026-03-20 21:21:18 +0000

    User edit code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -414,7 +414,7 @@ const Index = () => {
         </div>
       </nav>
 
-      <nav className="w-full bg-red-500 shadow-lg hidden position-absolute top-0 hover:visible">
+      <nav className="w-full bg-red-500 shadow-lg hidden position-absolute top hover:visible">
         <div className="mx-auto max-w-7xl px-4">
           <div className="flex items-center justify-between h-20">
             {/* Logo */}
