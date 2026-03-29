8ba2bdb37396ade0b374ff0c7272e63cd34f0e07
enter-pro <github@enter.pro>
2026-03-29 21:15:12 +0000

    User edit code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -60,7 +60,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <div className="flex-shrink-0">
-              <a href="/" className="text-2xl font-bold text-white">
+              <a {windows.location="/" ? href="" : href="/" className="text-2xl font-bold text-white">
                 <img
                   src="https://grazia-prod.oss-ap-southeast-1.aliyuncs.com/resources/uid_100009141/3339.jpg"
                   alt="Service To Security"
