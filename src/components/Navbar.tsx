d82ec2fd7472cd4116149521924e077b10cca751
enter-pro <github@enter.pro>
2026-03-29 21:20:16 +0000

    User edit code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Phone, Menu, X, ChevronDown } from "lucide-react";
 import { useState, useEffect } from "react";
+import { useLocation } from "react-router"
 
 const Navbar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -60,7 +61,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <div className="flex-shrink-0">
-              <a {window.location.pathname="/" ? href="" : href="/" className="text-2xl font-bold text-white">
+              <a {window.location.pathname='/' ? href="" : href="/" className="text-2xl font-bold text-white">
                 <img
                   src="https://grazia-prod.oss-ap-southeast-1.aliyuncs.com/resources/uid_100009141/3339.jpg"
                   alt="Service To Security"
